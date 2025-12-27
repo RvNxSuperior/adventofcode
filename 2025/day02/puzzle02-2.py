c43d4268a112e469ffb8014b711125b5b54ca057
RvNxSuperior <johannes.richling@icloud.com>
2025-12-27 21:41:17 +0100

    updated puzzle 02-2
    ok, actually deleted my old code this time haha

--- 2025/day02/puzzle02-2.py
+++ 2025/day02/puzzle02-2.py
@@ -99,34 +99,6 @@ for IdRange in inputList:
 
 
 
-
-    
-
-
-
-    #for sequenceLen in range(1, math.ceil(rangeLowLen/2) + 1):
-
-        #print(f'Sequence length: {sequenceLen}')
-
-        #sequenceStart = int(str(rangeLow)[0:sequenceLen - rangeLowLen])
-
-        #sequenceEnd = int(str(rangeHigh)[0:sequenceLen - rangeLowLen])
-
-        #print(sequenceStart)
-        #print(sequenceEnd)
-
-        #for sequence in range(sequenceStart, sequenceEnd + 1):
-
-            #sequenceTemp1 = int(str(sequence)*math.floor(rangeLowLen/sequenceLen))
-            #sequenceTemp2 = int(str(sequence)*(math.floor(rangeLowLen/sequenceLen) + 1))
-
-            #if rangeLow <= sequenceTemp1 <= rangeHigh:
-                #IdList.append(sequenceTemp1)
-
-            #if rangeLow <= sequenceTemp2 <= rangeHigh:
-                #IdList.append(sequenceTemp2)
-
-
 print(IdSet)
 
 sum = 0
