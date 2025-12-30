682d426d657f761d267c19e1b8de1d7c4be15cd6
RvNxSuperior <johannes.richling@icloud.com>
2025-12-30 14:42:39 +0100

    updated puzzle03-2
    just noted down some ideas for 03-2. will implement soon I guess

--- 2025/day03/puzzle03-2.py
+++ 2025/day03/puzzle03-2.py
@@ -22,3 +22,7 @@ for bank in inputList:
     print()
 
     
+# idea: use a function that, similarly to puzzle 03-1, gets the highest joltage in a bank,
+# but adjust the range in which the function searches
+# meaning that it looks for the highest joltage in the bank excluding the last 11 position, because a joltage there can't be used as the first joltage
+# and for every following joltage adjust the range to only search in the span between the joltage before and the joltage which can still be used
