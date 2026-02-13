5638eb43ccbbdca316b9ef8900902138dc81a616
RvNxSuperior <johannes.richling@icloud.com>
2026-02-13 12:22:41 +0100

    failed attempt on 07-2
    tried a brute force approach. Computational power is not sufficient

--- 2025/day07/puzzle07-2.py
+++ 2025/day07/puzzle07-2.py
@@ -30,10 +30,69 @@ def findall(string, substring):
             deltaString += 1
             start += len(substring)
 
-        
     return output
 
 
+def findleft(start = None):
+
+
+    if start != None:
+
+        output = start
+        startInt = len(start)
+        pos = SPosition + start.count('R') - start.count('L')
+
+
+    else:
+
+        output = ''
+        startInt = 0
+        pos = SPosition
+
+    
+    for stage in range(startInt, int(len(inputLines)/2)):
+
+        if inputLines[stage*2][pos] == '^':
+
+            pos -= 1
+            output += 'L'
+
+        else:
+
+            output += '-'
+
+    
+    return output
+
+print()
+print('Running...')
+
+SolutionSet = set()
+
+seed = findleft()
+count = 0
+
+
+while seed.count('L') > 0 and count <= 100000000:
+
+    count += 1
+
+    lastL = findall(seed, 'L')[-1]
+
+    current = findleft(seed[0:lastL] + 'R')
+
+    SolutionSet.add(current)
+    seed = current
+
+    #print(f'{count :<5}: {current}')
+
+    if count % 100000 == 0:
+        print(f'{count:<10}: {current}')
+
+
+print()
+print(f'{count :<5}: {current}')
+print(f'Solution count: {len(SolutionSet)}')
 
 
 # ideas: solve problem with depth first search approach
@@ -42,3 +101,6 @@ def findall(string, substring):
 # 2. finding the deepest splitter which hasn't been turned to a right split previously
 # loop finctions and store corresponding results in a set
 # CAVE: include spaces with no splitters in the solution to not end up with the same path-name for different paths
+
+# update: computational power is not sufficient for brute-force. Probably better to calculate the paths like in 07-1 but adjust for mutliple rays overlapping.
+# Maybe use a dictionary for the paths and multiply
